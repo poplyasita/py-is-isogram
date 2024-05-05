5296223da2f4b334db1fe6bda9525b265122681f
Olena Poplias <poplyasitasony@gmail.com>
2024-05-05 15:48:25 -0400

    "Solution"

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_string_with_different_cases_of_the_same_letter_is_not_an_isogram(
+) -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_not_only_consecutive_letters_are_not_an_isogram(
+) -> None:
+    assert is_isogram("oops") is False
+
+
+def test_not_only_non_consecutive_letters_are_not_an_isogram(
+) -> None:
+    assert is_isogram("look") is False
+
+
+def test_empty_string_is_an_isogram(
+) -> None:
+    assert is_isogram("") is True
